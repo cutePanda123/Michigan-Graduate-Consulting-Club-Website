d53e81a3912b4bb979a1ae2a5ae5244c5c29d035
Peter Li <longhanli123@gmail.com>
2018-01-30 22:20:00 -0800

    Added Join Us and Contact pages.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,15 @@
 $(document).ready(() => {
-    $(".nav a").click((event) => {
-        $(".nav").find(".active").removeClass("active");
+    $(".nav-item a").click((event) => {
+        $(".nav-item").find(".active").removeClass("active");
         $(this).parent().addClass("active");
     });
 });
+
+function sendMessage() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value + '%0D%0A' + `From: ${name}`;
+    var win = window.open(`mailto:zhilinch@umich.edu?subject=${subject} &body=${message}`);
+    if (win && win.open && !win.closed) win.close();
+}
